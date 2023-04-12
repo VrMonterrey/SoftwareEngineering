7b33d0b0e4c0012d72b4fb4d6a7a38d0ba4222b8
Oleksandr Chaban <toer1xe@gmail.com>
2023-04-12 13:32:55 +0200

    adapter posiłków

--- app/src/main/java/com/example/softwareengineering/DishAdapter.kt
+++ app/src/main/java/com/example/softwareengineering/DishAdapter.kt
@@ -1,10 +1,53 @@
-package com.example.softwareengineering
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.softwareengineering.R
+import com.example.softwareengineering.model.Posilki
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+class PosilkiAdapter(
+    private var posilkiList: MutableList<Posilki>,
+    private val listener: PosilkiAdapterListener
+) : RecyclerView.Adapter<PosilkiAdapter.PosilkiViewHolder>() {
 
-class DishAdapter : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PosilkiViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.posilki_item, parent, false)
+        return PosilkiViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PosilkiViewHolder, position: Int) {
+        val currentItem = posilkiList[position]
+
+        holder.nameTextView.text = currentItem.name
+        holder.categoryTextView.text = currentItem.category
+        holder.quantityTextView.text = currentItem.quantity.toString()
+    }
+
+    override fun getItemCount() = posilkiList.size
+
+    fun updateData(newPosilki: MutableList<Posilki>) {
+        posilkiList.clear()
+        posilkiList = newPosilki
+        notifyDataSetChanged()
+    }
+
+    inner class PosilkiViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.posilki_name)
+        val categoryTextView: TextView = itemView.findViewById(R.id.posilki_category)
+        val quantityTextView: TextView = itemView.findViewById(R.id.posilki_quantity)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener.onItemClick(posilkiList[position])
+                }
+            }
+        }
+    }
+
+    interface PosilkiAdapterListener {
+        fun onItemClick(posilki: Posilki)
     }
 }
--- app/src/main/java/com/example/softwareengineering/EditDish.kt
+++ app/src/main/java/com/example/softwareengineering/EditDish.kt
@@ -1,11 +1,186 @@
 package com.example.softwareengineering
 
+import android.app.AlertDialog
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.softwareengineering.adapter.SkladnikiToChooseAdapter
+import com.example.softwareengineering.model.Posilki
+import com.example.softwareengineering.model.Skladnik
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
-class EditDish : AppCompatActivity() {
+class EditDishActivity : AppCompatActivity() {
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var productRef: DatabaseReference
+    private lateinit var logout: ImageButton
+    private lateinit var home: ImageButton
+    private lateinit var categories: ImageButton
+    private lateinit var dishName: EditText
+    private lateinit var dishCategory: EditText
+    private lateinit var dishQuantity: EditText
+    private lateinit var addButton: ImageButton
+    private lateinit var dialogButton: Button
+
+    private lateinit var adapter: SkladnikiToChooseAdapter
+
+    private var selectedProducts: List<Skladnik> = emptyList()
+
+    private lateinit var imageView: ImageView
+    private lateinit var chooseImageButton: Button
+    private lateinit var galleryLauncher: ActivityResultLauncher<String>
+    private lateinit var photoUrl: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_dish)
+        setContentView(R.layout.activity_edit)
+
+        dialogButton = findViewById(R.id.dialog_btn)
+        dialogButton.setOnClickListener{ showCustomDialog() }
+
+        logout = findViewById(R.id.logout_button)
+        home = findViewById(R.id.home_button)
+        categories = findViewById(R.id.categories_btn)
+
+        addButton = findViewById(R.id.submit_btn)
+
+        val database = Firebase.database.reference
+
+        imageView = findViewById<ImageView>(R.id.image_view)
+        chooseImageButton = findViewById<Button>(R.id.choose_image_button)
+
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            if (uri != null) {
+                imageView.setImageURI(uri)
+
+                val currentUser = FirebaseAuth.getInstance().currentUser
+                val currentUserId = currentUser?.uid ?: ""
+                val dishesRef = database.child("dishes")
+                val newDishRef = dishesRef.push()
+                val newDishKey = newDishRef.key
+                photoUrl = "$currentUserId/dishes/$newDishKey/photo.jpg"
+            }
+        }
+
+        home.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+
+        categories.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(applicationContext, CategoriesActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+
+        logout.setOnClickListener(View.OnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            var intent: Intent = Intent(applicationContext, login::class.java)
+            startActivity(intent)
+            finish()
+        })
+
+        chooseImageButton.setOnClickListener {
+            galleryLauncher.launch("image/*")
+        }
+
+    }
+
+    private fun showCustomDialog() {
+        val builder = AlertDialog.Builder(this)
+        val dialogLayout = LayoutInflater.from(this).inflate(R.layout.dialog_layout, null)
+        val recyclerView = dialogLayout.findViewById<RecyclerView>(R.id.ingredients_rv)
+
+        val database = FirebaseDatabase.getInstance()
+        val productsRef = database.getReference("products")
+        val productsListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val products = dataSnapshot.children.mapNotNull { it.getValue(Skladnik::class.java) }
+                adapter.setData(products)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "loadIngredients:onCancelled", databaseError.toException())
+            }
+        }
+        productsRef.addValueEventListener(productsListener)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val products = mutableListOf<Skladnik>()
+        adapter = SkladnikiToChooseAdapter(products) { product ->
+            if (product.checked) {
+                products.add(product)
+            } else {
+                products.remove(product)
+            }
+        }
+
+        recyclerView.adapter = adapter
+
+
+        builder.setTitle("Custom Dialog")
+            .setMessage("This is a custom dialog.")
+            .setView(dialogLayout)
+            .setPositiveButton("OK") { dialog, which ->
+                selectedProducts = adapter.getData().filter { it.checked }
+                Log.d(TAG, "Selected products: $selectedProducts")
+            }
+            .setNegativeButton("Cancel") { dialog, which ->
+
+            }
+            .create()
+            .show()
+
+        addButton.setOnClickListener {
+            dishName = findViewById<EditText>(R.id.name_edit_text)
+            dishCategory = findViewById<EditText>(R.id.category_edit_text)
+            dishQuantity = findViewById<EditText>(R.id.ilosc_edit_text)
+
+            val name = dishName.text.toString()
+            val category = dishCategory.text.toString()
+            val quantity = dishQuantity.text.toString().toInt()
+
+            val database = Firebase.database.reference
+
+            val dish = Posilki(
+                id = database.child("dishes").push().key,
+                name = name,
+                category = category,
+                quantity = quantity,
+                products = selectedProducts,
+                photoUrl = photoUrl
+            )
+
+            if (dish.id != null) {
+                database.child("dishes").child(dish.id!!).setValue(dish).addOnSuccessListener {
+                    Toast.makeText(this, "Nowy posiłek dodany pomyślnie", Toast.LENGTH_SHORT).show()
+                    dishName.text.clear()
+                    dishCategory.text.clear()
+                    dishQuantity.text.clear()
+                    imageView.setImageBitmap(null)
+                }
+                    .addOnFailureListener {
+                        Toast.makeText(
+                            this,
+                            "Błąd podczas dodawania posiłka: ${it.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            }
+        }
     }
 }
